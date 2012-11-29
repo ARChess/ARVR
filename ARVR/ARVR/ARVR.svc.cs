4e40886671fe1a1bf430d64a2e39ac00654aa573
Ethan McGee <bulletshot60@gmail.com>
2012-11-28 22:30:58 -0500

    init voice recognizer

---
+++ ARVR/ARVR/ARVR.svc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Speech.Recognition;
+using System.Speech.AudioFormat;
+using System.Threading;
+
+namespace ARVR
+{
+    public class ARVR : IARVR
+    {
+        private Grammar _grammar = null;
+
+        public string RecognizeSpeech(byte[] speechToParse, int sampleRate)
+        {
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+
+            if (_grammar == null)
+                InitializeGrammar();
+            sre.LoadGrammar(_grammar);
+
+            MemoryStream ms = new MemoryStream(speechToParse);
+            var formatInfo = new SpeechAudioFormatInfo(sampleRate, AudioBitsPerSample.Sixteen, AudioChannel.Mono);
+            sre.SetInputToAudioStream(ms, formatInfo);
+            var result = sre.Recognize();
+            ms = null;
+
+            if (result == null)
+                return "Unable to recognize speech";
+            else
+                return result.Text;
+        }
+
+        private void InitializeGrammar()
+        {
+            //[select <piece> at <x><y> | move <piece> to <x><y>]
+
+            //build the core set of choices
+            Choices action = new Choices("select", "move", "select the", "move the");
+            Choices pieces = new Choices("pawn", "rook", "bishop", "knight", "king", "queen");
+            Choices x_positions = new Choices("A", "B", "C", "D", "E", "F", "G", "H");
+            Choices y_positions = new Choices("1", "2", "3", "4", "5", "6", "7", "8");
+
+            //now build the complete pattern...
+            GrammarBuilder commandRequest = new GrammarBuilder();
+            commandRequest.Append(action);
+            commandRequest.Append(pieces);
+            commandRequest.Append(new Choices("at", "to"));
+            commandRequest.Append(x_positions);
+            commandRequest.Append(y_positions);
+
+            //create the pizza grammar
+            _grammar = new Grammar(commandRequest);
+        }
+    }
+}
